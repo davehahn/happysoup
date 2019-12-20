6aaeee437d52aa52645f2f707341fdfb494fb7aa
Dave Hahn <dave@davehahn.me>
2019-12-20 10:08:17 -0500

    typo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 echo $1 | base64 --decode > deploy.key
 sfdx force:auth:jwt:grant -i $3 -u $2 -f deploy.key --setalias ci-deploy --instanceurl $4
-./local_deploy.sh ci-deploy RunLocalTest
+./local_deploy.sh ci-deploy RunLocalTests
