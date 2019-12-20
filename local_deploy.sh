ba4edc5bcfcc90b08615dbe7ba764011f1e1fa1f
Dave Hahn <dave@davehahn.me>
2019-12-20 15:35:52 -0500

    updated deploy scripts again

--- local_deploy.sh
+++ local_deploy.sh
@@ -26,25 +26,7 @@ cmd=$(pwd)
 mkdir ~/.deploy_root
 cd ~/.deploy_root
 git clone $cmd .
-echoOut 'Removing Lead list views'
-rm -fR force-app/main/default/objects/Lead/listViews
-echoOut 'Removing Opportunity list views'
-rm -fR force-app/main/default/objects/Opportunity/listViews
-echoOut 'Removing Lead IqScore field'
-rm -f force-app/main/default/objects/Lead/fields/IqScore.field-meta.xml
-echoOut 'Removing Opportunity IqScore field'
-rm -f force-app/main/default/objects/Opportunity/fields/IqScore.field-meta.xml
-echoOut 'Removing troublesome LiveChatTranscript layouts'
-rm -f force-app/main/default/layouts/LiveChatTranscriptActive-Live\ Chat\ Transcript\ %28In\ Progress%29\ Layout.layout-meta.xml
-rm -f force-app/main/default/layouts/LiveChatTranscriptWaiting-Live\ Chat\ Transcript\ %28Waiting%29\ Layout.layout-meta.xml
-echoOut 'Removing Accounting Home1 tab'
-rm -f force-app/main/default/tabs/AcctSeed__Accounting_Home1.tab
-echoOut 'Running ant doMetadataClean'
-ant -buildfile build/build.xml doMetadataClean
-git commit -am 'post metadata clean'
-echoOut 'Deploying static resources'
-sfdx heber:staticresources:deploy -u $1
-echo 'Deploying the remaining metadata'
-sfdx force:source:deploy --testlevel $TESTLEVEL --targetusername $1 -p force-app/main/default -w 120
+chmod +x deploy.sh
+./deploy.sh $1 $TESTLEVEL
 echoOut 'Ceaning up'
 cd .. && rm -fR .deploy_root
