dd17ed03f49d566f333f4c51ad6ca46940cc9e1e
Dave Hahn <dave@davehahn.me>
2019-12-20 10:03:19 -0500

    restructured pipeline file and deploy scripts
    restructure deploy scripts

---
+++ local_deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+BLUE=`tput setaf 4`
+RESET=`tput sgr0`
+TESTLEVEL='RunLocalTest'
+
+echoOut() {
+  echo "${BLUE}$1${RESET}"
+}
+
+if [[ -z $1 ]]; then
+  echo "You need to supply the sfdx org alias which you are trying to deploy to"
+  exit 1
+fi
+
+if [ -z $2 ]
+then
+  TESTLEVEL='NoTestRun'
+  echoOut 'Deployment running with NO TESTS - Sandbox ONLY'
+else
+  TESTLEVEL=$2
+  echoOut "Deployment running with --testlevel=$2"
+fi
+
+cmd=$(pwd)
+mkdir ~/.deploy_root
+cd ~/.deploy_root
+git clone $cmd .
+echoOut 'Removing Lead list views'
+rm -fR force-app/main/default/objects/Lead/listViews
+echoOut 'Removing Opportunity list views'
+rm -fR force-app/main/default/objects/Opportunity/listViews
+echoOut 'Removing Lead IqScore field'
+rm -f force-app/main/default/objects/Lead/fields/IqScore.field-meta.xml
+echoOut 'Removing Opportunity IqScore field'
+rm -f force-app/main/default/objects/Opportunity/fields/IqScore.field-meta.xml
+echoOut 'Removing troublesome LiveChatTranscript layouts'
+rm -f force-app/main/default/layouts/LiveChatTranscriptActive-Live\ Chat\ Transcript\ %28In\ Progress%29\ Layout.layout-meta.xml
+rm -f force-app/main/default/layouts/LiveChatTranscriptWaiting-Live\ Chat\ Transcript\ %28Waiting%29\ Layout.layout-meta.xml
+echoOut 'Removing Accounting Home1 tab'
+rm -f force-app/main/default/tabs/AcctSeed__Accounting_Home1.tab
+echoOut 'Running ant doMetadataClean'
+ant -buildfile build/build.xml doMetadataClean
+git commit -am 'post metadata clean'
+echoOut 'Deploying static resources'
+sfdx heber:staticresources:deploy -u $1
+echo 'Deploying the remaining metadata'
+sfdx force:source:deploy --testlevel $TESTLEVEL --targetusername $1 -p force-app/main/default -w 120
+echoOut 'Ceaning up'
+cd .. && rm -fR .deploy_root
