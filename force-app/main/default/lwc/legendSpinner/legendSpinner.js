b6dbe93a2b3719b0e2bb27646e54d86bee5ed7b1
Dave Hahn <dave@davehahn.me>
2020-01-24 16:51:41 -0500

    begin building Commission 2 Structure
    end of day Jan27
    
    temp commit
    
    temp commit
    
    temp commit
    
    temp commit
    
    temp commit
    
    Unit testing of CommissionRecord2 and CommissionlineItem2
    
    temp commit
    
    temp commit
    
    fixed some things
    
    typo
    
    temp commit
    
    retrieved triggers
    
    actuall add the file now
    
    temp commit
    
    temp commit
    
    temp commit
    
    temp commit
    
    commission payments now functional
    
    samll typo
    
    all erps now have Uses_V2_Commissions__c set to true in trigger on before insert
    
    retrieved missing fields on CommissionRecord2__c
    
    payment UI and structure complete
    
    temp commit
    
    temp commit
    
    all done but some permissions
    
    refreshed AcctSeed__Project__c object
    
    fixed one unit test and added a validation rule to Insurance Plans
    
    fix acouple more tests
    
    small change to deploy scripts
    
    fixed a few more tests
    
    pull AcctSeed__Project__c from prod
    
    begin writing sync code between Materials and CommissionLineItems ** TEST CURRENTLY BROKEN
    
    Commission v2 - beat1 complete

--- force-app/main/default/lwc/legendSpinner/legendSpinner.js
+++ force-app/main/default/lwc/legendSpinner/legendSpinner.js
@@ -6,7 +6,7 @@ import { LightningElement, api, track } from 'lwc';
 import SPINNER from '@salesforce/resourceUrl/LegendLoadingIndicator2017';
 
 export default class LegendSpinner extends LightningElement {
-  @track visible = false;
+  @api visible = false;
   @api variant;
   spinner = SPINNER;
 
