9b42cef5240249c75a3cc0cc79550cfad095edb8
Dave Hahn <dave@davehahn.me>
2020-04-09 17:39:18 -0400

    added community pages for Boats (3) and a My Home page wich is protected

--- force-app/main/default/lwc/custCommPublicMain/custCommPublicMain.js
+++ force-app/main/default/lwc/custCommPublicMain/custCommPublicMain.js
@@ -3,16 +3,60 @@
  */
 
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import LOGO from '@salesforce/resourceUrl/LegendLogo';
 
-export default class CustCommPublicMain extends LightningElement {
+export default class CustCommPublicMain extends NavigationMixin(LightningElement) {
 
   logo = LOGO;
+  pageMap = [
+    {
+      pageName: 'boat1page',
+      recordId: 'recordIdOne'
+    },
+    {
+      pageName: 'boat2page',
+      recordId: 'recordIdTwo'
+    },
+    {
+      pageName: 'boat3page',
+      recordId: 'recordIdThree'
+    }
+  ]
 
   toggleMenu()
   {
     this.template.querySelector('.overlay').classList.toggle('open');
     this.template.querySelector('.slide-menu').classList.toggle('open');
+  }
+
+  navToBoat( event )
+  {
+    event.preventDefault();
+    let page = this.pageMap[event.target.dataset.menuNumber],
+        params = {
+          recordId: page.recordId
+        };
+
+    this.navigateToCommunityPage( page.pageName, params );
+  }
+
+  handleSignIn( event )
+  {
+    this.navigateToCommunityPage( 'my-home', {} );
+  }
+
+  navigateToCommunityPage( pageName, params )
+  {
+    console.log( pageName );
+    console.log( params );
+    this[NavigationMixin.Navigate]({
+        type: 'comm__namedPage',
+        attributes: {
+            pageName: pageName
+        },
+        state: params
+    });
   }
 
 }
