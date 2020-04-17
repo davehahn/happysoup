7aba2c252b701a950983affd5e2867ae7d5828c3
Dave Hahn <dave@davehahn.me>
2020-04-17 09:45:29 -0400

    Square Payment form now in its own LWC

---
+++ force-app/main/default/lwc/squarePaymentForm/squarePaymentForm.js
@@ -0,0 +1,69 @@
+/**
+ * Created by dave on 2020-04-16.
+ */
+
+import { LightningElement, api, track } from 'lwc';
+
+export default class SquarePaymentForm extends LightningElement {
+
+  origin;
+
+  @api apexPageName;
+  @api isResponsive = false;
+  @api widthBreakPoint = 140;
+  @api smallHeight = 200;
+  @api largeHeight = 400;
+
+  @track iframeHeight;
+
+  constructor()
+  {
+    super();
+    this.origin = window.location.origin;
+  }
+
+  connectedCallback()
+  {
+    if( this.isResponsive )
+    {
+      window.addEventListener('resize', (event) => {
+        this.setIframeHeight( event.currentTarget.outerWidth );
+      });
+    }
+    this.setIframeHeight( window.outerWidth );
+  }
+
+  @api doPostToSquare( paymentAmount )
+  {
+    const data = {
+      messageType: 'doPayment',
+      paymentAmount: paymentAmount
+    };
+    const iframe = this.template.querySelector('[data-id="square-payment-container"]').contentWindow;
+
+    return new Promise( (resolve, reject) => {
+      const messageHandler = ( event ) =>
+      {
+        if( event.origin === this.origin )
+        {
+          window.removeEventListener('message', messageHandler );
+          resolve( event.data );
+        }
+      };
+      window.addEventListener('message', messageHandler );
+      iframe.postMessage(data, this.origin);
+    });
+  }
+
+  get pageURL()
+  {
+    const urlString = window.location.href;
+    return urlString.substring(0, urlString.indexOf("/s") ) + this.apexPageName;
+  }
+
+  setIframeHeight( width )
+  {
+     this.iframeHeight = width >= this.widthBreakPoint ?
+      `height:${this.smallHeight}px` : `height:${this.largeHeight}px`;
+  }
+}
