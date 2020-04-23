ac09a5ed9993b2f99b967ff53fae16423cf0c147
Tim Lamond <tlamond@legendboats.com>
2020-04-23 14:14:03 -0400

    Additon of currCommOrderBoatImage to handle the main boat image on all pages.

---
+++ force-app/main/default/lwc/custCommOrderBoatImage/custCommOrderBoatImage.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Tim on 2020-04-23.
+ */
+
+import { LightningElement, api } from 'lwc';
+
+export default class CustCommOrderBoatImage extends LightningElement {
+    @api size;
+}
