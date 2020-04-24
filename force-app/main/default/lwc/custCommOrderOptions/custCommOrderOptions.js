9786c174ea3bb5bf0d91c407d70af0e9e1e0a6fa
Tim Lamond <tlamond@legendboats.com>
2020-04-24 08:12:21 -0400

    Refinement of Boat Image component; Addition of order Summary component.

--- force-app/main/default/lwc/custCommOrderOptions/custCommOrderOptions.js
+++ force-app/main/default/lwc/custCommOrderOptions/custCommOrderOptions.js
@@ -4,17 +4,9 @@
 
 import { LightningElement, api } from 'lwc';
 
-import { loadStyle } from 'lightning/platformResourceLoader';
-import gothamFonts from '@salesforce/resourceUrl/GothamHTF';
-
 export default class CustCommOrderOptions extends LightningElement {
     @api optionsTitle;
     @api selections;
     @api selectionScope;
 
-    renderedCallback()
-    {
-        loadStyle( this, gothamFonts + '/fonts.css')
-        .then(()=>{});
-     }
 }
---
+++ force-app/main/default/lwc/custCommOrderSummary/custCommOrderSummary.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Tim on 2020-04-23.
+ */
+
+import { LightningElement } from 'lwc';
+
+export default class CustCommOrderSummary extends LightningElement {
+
+}
