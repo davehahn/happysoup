e3e44c9d04915f89319e1587f4d8617e04445ef5
Amin Rubel <rubel@legendboats.com>
2020-04-27 12:08:58 +0600

    Duplicate checking for Person Account from Retail Sales Tool

--- force-app/main/default/aura/Retail_Sales_Tool_PersonForm_dh/Retail_Sales_Tool_PersonForm_dhHelper.js
+++ force-app/main/default/aura/Retail_Sales_Tool_PersonForm_dh/Retail_Sales_Tool_PersonForm_dhHelper.js
@@ -67,6 +67,82 @@
     {
       return Promise.resolve(oppId);
     }
-  }
+  },
+
+  checkDuplicate: function( component )
+  {
+    var action = component.get('c.checkDuplicate'), la;
+    action.setParams({
+      customerJSON: JSON.stringify(component.get('v.customer') )
+    });
+    la = new LightningApex( this, action );
+    return la.fire();
+  },
+
+  handleDuplicate: function( component, event, helper )
+  {
+    var steps = component.get('v.steps'),
+        step = component.get('v.currentStep'),
+        stepNum = steps.indexOf(step),
+        spinner = component.find('spinner');
+
+      spinner.toggle();
+      helper.checkDuplicate(component)
+      .then(
+        $A.getCallback( function( result ) {
+          //if(result != undefined || result != null)
+          //  result = JSON.parse(result);
+          component.set("v.showPop", true);
+          component.set('v.listData', result );
+          console.log(result);
+        })
+      )
+      .finally( $A.getCallback( function() {
+        spinner.toggle();
+      }));
+  },
+  closeModel: function(component, event, helper) {
+    component.set("v.showPop", false);
+  },
+  handleContinue: function( component, event, helper )
+      {
+        var steps = component.get('v.steps'),
+            step = component.get('v.currentStep'),
+            stepNum = steps.indexOf(step) + 1,
+            spinner = component.find('spinner');
+
+        if( helper.formValid( component, stepNum ) )
+        {
+          spinner.toggle();
+          var cont = this;
+          helper.createSale( component )
+          .then(
+            $A.getCallback( function( result ) {
+              component.set('v.opportunityId', result );
+              var evt = $A.get("e.c:Retail_Sales_Tool_OppCreated_Event");
+              evt.setParams({
+                opportunityId: result
+              })
+              .fire();
+            }),
+            $A.getCallback( function( err ) {
+              var duplicateMessage = err;//'DUPLICATES_DETECTED';
+                          console.log(err);
+                if(duplicateMessage.includes('DUPLICATES_DETECTED') || duplicateMessage.includes('Duplicate Alert')){
+                    console.log('DUPLICATES_DETECTED');
+                  console.log(duplicateMessage);
+                  helper.handleDuplicate(component, event, helper);
+                }else{
+                  LightningUtils.errorToast( err );
+                }
+            })
+          )
+          .finally( $A.getCallback( function() {
+            spinner.toggle();
+          }));
+        }
+        else
+          LightningUtils.errorToast("Please fix any errors on the form to continue");
+      }
 
 })
