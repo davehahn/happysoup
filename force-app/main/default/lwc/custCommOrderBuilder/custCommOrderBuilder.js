c2dc829511f8c1d971af91fac9f03aa13095a526
Tim Lamond <tlamond@legendboats.com>
2020-04-29 12:25:34 -0400

    Beginning work on Show Details modal; addition of Legend Logo Vertical to static resources.

--- force-app/main/default/lwc/custCommOrderBuilder/custCommOrderBuilder.js
+++ force-app/main/default/lwc/custCommOrderBuilder/custCommOrderBuilder.js
@@ -9,6 +9,7 @@ import { loadStyle } from 'lightning/platformResourceLoader';
 import sldsIconFont from '@salesforce/resourceUrl/sldsIconFont';
 import gothamFonts from '@salesforce/resourceUrl/GothamHTF';
 import LOGO from '@salesforce/resourceUrl/LegendLogo';
+import VLOGO from '@salesforce/resourceUrl/LegendLogoVertical';
 
 export default class CustCommOrderBuilder extends NavigationMixin(LightningElement) {
 
@@ -23,6 +24,26 @@ export default class CustCommOrderBuilder extends NavigationMixin(LightningEleme
     'payment'
   ];
   @track currentPage = 'performance';
+
+ 	 modalPages = [
+		{
+			title: 'Premium Package',
+			label: 'premium-package',
+			class: 'modal-nav-item modal-nav-item_selected'
+		},
+		{
+			title: 'Payment Calculator',
+			label: 'payment-calculator',
+			class: 'modal-nav-item'
+		},
+		{
+			title: 'Deliver Timing',
+			label: 'delivery',
+			class: 'modal-nav-item'
+		},
+	];
+	@track currentModalPage = 'premium-package';
+
   @track paymentType='cash';
   @track iframeHeight;
   isMobile = false;
@@ -47,24 +68,6 @@ export default class CustCommOrderBuilder extends NavigationMixin(LightningEleme
     loadStyle( this, sldsIconFont + '/style.css')
     .then(()=>{});
     loadStyle( this, gothamFonts + '/fonts.css')
-    .then(()=>{});
-  }
-
-  get processPages()
-  {
-    return this.pages.map( page => {
-      return {
-        label: page,
-        class: this.currentPage === page ?
-          'config-nav-item config-nav-item_selected' :
-          'config-nav-item'
-      }
-    });
-  }
-
-  renderedCallback()
-  {
-    loadStyle( this, sldsIconFont + '/style.css')
     .then(()=>{});
   }
 
@@ -125,10 +128,28 @@ export default class CustCommOrderBuilder extends NavigationMixin(LightningEleme
   handleOpenModal()
   {
     this.toggleModal( true );
-    this.template.querySelector('.modal-container').addEventListener('click', () => {
-      this.toggleModal(false);
+    this.template.querySelector('.modal-container').addEventListener('click', (e) => {
+      if(e.target.classList.contains('modal-container')){
+      	this.toggleModal(false);
+      }
     });
   }
+
+	handleModalNav( event )
+	{
+		this.doModalPageChange( event.currentTarget );
+	}
+	doModalPageChange( page )
+	{
+	  let pageName = page.dataset.modalNavName;
+		this.currentModalPage = pageName;
+
+		this.template.querySelector('.modal-nav-item_selected').classList.remove('modal-nav-item_selected');
+		this.template.querySelector( `[data-modal-nav-name="${this.currentModalPage}"]` ).classList.add('modal-nav-item_selected');
+
+		this.template.querySelector('.modal-page_selected').classList.remove('modal-page_selected');
+		this.template.querySelector( `[data-modal-page="${this.currentModalPage}"]` ).classList.add('modal-page_selected');
+	}
 
   handleEditConfig()
   {
