5eae75ca39e161095a6f5601340f97acb436e403
Dave Hahn <dave@davehahn.me>
2020-07-23 10:57:17 -0400

    fix small issue in validateBuild.sh

--- validateBuild.sh
+++ validateBuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #accessToken="$(cat $BITBUCKET_CLONE_DIR/org_info.json | jq -r  '.accessToken')"
+DEPLOY_EXPERIENCES=false
 USER="$(cat $BITBUCKET_CLONE_DIR/org_info.json | jq -r '.username')"
 echo "$USER"
 sfdx sfpowerkit:auth:login -u $USER --password $1 -a ci-testValid
@@ -30,7 +31,20 @@ rm -f force-app/main/default/objects/Contact/webLinks/mcdm_15__*
 echo 'Running ant doMetadataClean'
 ant -buildfile build/build.xml doMetadataClean
 
-echo 'modifying .forceignore to not deploy staticresources'
+echo 'make a copy of .forceignore'
+cp .forceignore .forceignore.orig
+
+echo 'modifying .forceignore to not deploy staticresources and experienceBundles'
 echo -e "\nforce-app/main/default/staticresources" >> .forceignore
+echo -e "\nforce-app/main/default/experiences" >> .forceignore
+
 
 sfdx force:source:deploy --testlevel RunLocalTests --checkonly --targetusername ci-testValid -p force-app/main/default -g -w 120
+if [ $DEPLOY_EXPERIENCES = true ]
+then
+  echoOut 're-enable original .forceignore'
+  rm -f .forceignore
+  mv .forceignore.orig .forceignore
+  echoOut 'Deploying ExperienceBundles'
+  sfdx force:source:deploy --testlevel NoTestRun --checkonly --targetusername ci-testValid -p force-app/main/default/experiences -g -w 120
+fi
