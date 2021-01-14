559f4927c5fae2441bb4156c50d2e5ce4fa08e20
Dave Hahn <dhahn@legendboats.com>
2021-01-14 16:57:53 -0500

    Closes SFISSUES-407 - can now show/hide Factory Store Inventories in the Partner2Partner Inventory Search via a design attribute on the component

--- force-app/main/default/aura/P2PInventorySearch_mp/P2PInventorySearch_mpHelper.js
+++ force-app/main/default/aura/P2PInventorySearch_mp/P2PInventorySearch_mpHelper.js
@@ -2,6 +2,9 @@
 	searchInventories : function(component) {
     console.log('helper.search')
 		var action = component.get('c.searchPartnerInventories');
+		action.setParams({
+		  includeFactoryStoresForPartners: component.get('v.includeFactoryStoresForPartners')
+    });
     return new LightningApex( this, action ).fire();
 	}
 })
