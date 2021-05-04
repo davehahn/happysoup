fe891c59ebaa3241a04608d5432ae5b1eda67ec3
Tim Lamond <tlamond@legendboats.com>
2021-05-03 23:13:29 -0400

    Continued work on lead form component.

--- force-app/CommunityShared/lwc/communitySharedLeadForm/communitySharedLeadForm.js
+++ force-app/CommunityShared/lwc/communitySharedLeadForm/communitySharedLeadForm.js
@@ -4,11 +4,33 @@
 
 import { LightningElement, wire, api, track } from 'lwc';
 import insertLead from '@salesforce/apex/CommSharedLeadForm_Controller.insertLead';
+import LeadForm_FirstName from '@salesforce/label/c.LeadForm_FirstName';
+import LeadForm_LastName from '@salesforce/label/c.LeadForm_LastName';
 import { getTestUser } from 'c/communitySharedUtils';
 
 export default class CommunitySharedLeadForm extends LightningElement {
 
-	@api campaignId;
+	@api formName;
+  @api campaignId;
+  @api collectFirstName;
+  @api collectLastName;
+  @api collectStreet;
+  @api collectCity;
+  @api collectPostalCode;
+  @api collectProvince;
+  @api collectEmail;
+  @api collectPhone;
+  @api collectPlanToPurchase;
+  @api collectNotes;
+  @api collectNewsletterOptin;
+  @api collectBoatType;
+  @api collectBoatModel;
+
+  labels = {
+    LeadForm_FirstName,
+    LeadForm_LastName
+  }
+
 	defaultCampaignId = '701q0000000mL7GAAU';
 
 	submitLead(){
