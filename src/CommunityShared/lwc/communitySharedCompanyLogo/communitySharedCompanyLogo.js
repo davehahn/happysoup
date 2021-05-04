e2dd5660d4654b587628a10172687e0f955d9246
Tim Lamond <tlamond@legendboats.com>
2021-05-04 09:30:24 -0400

    Moved missing components and classes from old forceapp structure to new src structure.

--- src/CommunityShared/lwc/communitySharedCompanyLogo/communitySharedCompanyLogo.js
+++ src/CommunityShared/lwc/communitySharedCompanyLogo/communitySharedCompanyLogo.js
@@ -2,50 +2,23 @@
  * Created by Tim on 2021-05-03.
  */
 
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import Id from '@salesforce/community/Id';
-import { NavigationMixin } from 'lightning/navigation';
-import fetchCommunityDetails from '@salesforce/apex/CommSharedURL_Controller.fetchCommunityDetails';
+import getManagedContentByContentKeys from '@salesforce/apex/CommSharedMC_Controller.getManagedContentByContentKeys';
 
-export default class CommunitySharedCompanyLogo extends NavigationMixin(LightningElement) {
+export default class CommunitySharedCompanyLogo extends LightningElement {
 	@api companyLogo;
-	@api companyName;
-	companyLogoBg;
-	companyLogoImg;
-
-	communityHomePageRef;
-	url;
-
-	connectedCallback(){
-	  this.communityHomePageRef = {
-	    type: 'comm__namedPage',
-	    attributes: {
-	      name: 'Home'
-     }
-   };
-   this[NavigationMixin.GenerateUrl](this.communityHomePageRef)
-   	.then(url => {
-			this.url = url;
-			console.log('this communityHomePageRef url: ', this.url);
-		});
- }
-
- navigateToPage( e ){
-   e.preventDefault();
-   e.stopPropagation;
-
-   this[NavigationMixin.Navigate](this.communityHomePageRef);
- }
+	@track companyLogoBg;
 
 	renderedCallback(){
 		if(this.companyLogo){
-		  fetchCommunityDetails({communityId: Id})
-		  	.then( (result) => {
-		  	  console.log('fetch community url result: ', result);
-		  	  this.companyLogoImg = 'background-image: url("' + result.siteUrl + '/cms/delivery/media/' + this.companyLogo + '")';
-				 }).catch(e => {
-					 console.log('fetch community url error: ', e);
-				 });
+		  getManagedContentByContentKeys({ communityId: Id, managedContentIds: this.companyLogo, pageParam: 0, pageSize: 1, language: 'en_US', managedContentType: 'cms_image', showAbsoluteUrl: false })
+				.then( (result) => {
+					this.companyLogoBg = 'background-image: url("' + result.items[0].contentNodes.source.url + '")';
+					console.log('result: ', result);
+				}).catch(e => {
+						console.log('error: ', e);
+				});
   	}
  	}
 }
