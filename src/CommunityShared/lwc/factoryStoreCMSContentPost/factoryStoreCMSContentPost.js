bee2f8be3976d5fb46e34625ae1fcaa3878a1bf6
Tim Lamond <tlamond@legendboats.com>
2021-05-10 10:19:41 -0400

    Merge conflict resolution.

--- src/CommunityShared/lwc/factoryStoreCMSContentPost/factoryStoreCMSContentPost.js
+++ src/CommunityShared/lwc/factoryStoreCMSContentPost/factoryStoreCMSContentPost.js
@@ -5,6 +5,8 @@
 import { LightningElement, wire, api, track } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import initMethod from '@salesforce/apex/FactoryStore_MCWrapperController.initMethod';
+import basePath from '@salesforce/community/basePath';
+import { decodeHTML } from 'c/communitySharedUtils';
 
 export default class FactoryStoreCmsContentPost extends LightningElement {
 
@@ -12,6 +14,14 @@ export default class FactoryStoreCmsContentPost extends LightningElement {
   @track post = '';
 
   @track postTitle;
+  @track bannerGraphic;
+  @track startDate;
+  @track endDate;
+  @track mainContent;
+  @track clickToFullDetails = false;
+  @track fullDetailsLink;
+
+  baseDir = (basePath.includes('/s')) ? basePath.slice(0, -2) : basePath;
 
   @wire(CurrentPageReference) currentPageReference;
 
@@ -45,6 +55,12 @@ export default class FactoryStoreCmsContentPost extends LightningElement {
  	renderPost(){
  	  const post = this.post;
  	  console.log('readable post: ', JSON.parse( JSON.stringify( post ) ) );
- 	  this.postTitle = post.title;
+ 	  this.postTitle = (post.title) ? post.title : 'Post Title Missing';
+ 	  this.bannerGraphic = (post.contentNodes.BannerGraphic.unauthenticatedUrl) ? 'background-image: url("' + this.baseDir + post.contentNodes.BannerGraphic.unauthenticatedUrl + '")' : 'Banner Graphic Missing';
+ 	  this.startDate = (post.contentNodes.StartDate.value) ? new Date(post.contentNodes.StartDate.value).toLocaleDateString() : 'Start Date Missing';
+ 	  this.endDate = (post.contentNodes.EndDate.value) ? new Date(post.contentNodes.EndDate.value).toLocaleDateString() : 'Ongoing';
+ 	  this.mainContent = (post.contentNodes.MainContent.value) ? decodeHTML(post.contentNodes.MainContent.value) : 'Main Content Missing';
+ 	  this.clickToFullDetails = (post.contentNodes.CampaignId.value) ? true : false;
+ 	  this.fullDetailsLink = (post.contentNodes.CampaignId.value) ? "https://www.legendboats.com/campaignDirect/" + post.contentNodes.CampaignId.value : '';
   }
 }
