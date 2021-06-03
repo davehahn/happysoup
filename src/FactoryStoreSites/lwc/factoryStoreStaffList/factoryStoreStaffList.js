89656d378fdf7ac80e2404d65c278b4304bb82fe
Tim Lamond <tlamond@legendboats.com>
2021-06-03 12:56:03 -0400

    Various layout revisions and updates; addition of Store Toggle component; redesign of Staff List Card.

--- src/FactoryStoreSites/lwc/factoryStoreStaffList/factoryStoreStaffList.js
+++ src/FactoryStoreSites/lwc/factoryStoreStaffList/factoryStoreStaffList.js
@@ -25,7 +25,7 @@ export default class FactoryStoreStaffList extends LightningElement {
 		{
 			if( data )
 			{
-				console.log('user Data: ', data);
+
 				this.showUserListing = true;
 				this.showLoading = false;
 				this.wrapperClass = setWrapperClass(this.sectionWidth, 'staff');
@@ -37,10 +37,16 @@ export default class FactoryStoreStaffList extends LightningElement {
 							let newData = data.map((item) => ({
 								...item,
 								ProfileImgUrl: trunkCommUrl + item.MediumPhotoUrl,
-								EmailLink: 'mailTo:' + item.Email,
-								PhoneLink: 'tel:' + item.Phone
+								EmailTrunk: item.Email.replace('.invalid', ''),
+								EmailLink: 'mailTo:' + item.Email.replace('.invalid', ''),
+								PhoneLink: 'tel:' + item.Phone,
+								Hometown: 'Mississauga',
+								StartYear: '2019',
+								Story: 'Quisque dapibus Leo ut mauris ultricies consectetur sit amet non leo. Pellentesque quis velit elit. Donec consectetur sem vitae lorem ultrices, eu fermentum augue fermentum. Duis non porttitor metus. Cras vel velit enim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+								LandscapeImg: 'background-image: url("https://mk0inventorylegv1t7j.kinstacdn.com/wp-content/uploads/2020/05/LegendLandscape-12.jpg")'
 							}));
 							this.users = newData;
+							console.log('user newData: ', newData);
 					 }).catch(e => {
 						 console.log('fetch community url error: ', e);
 					 });
