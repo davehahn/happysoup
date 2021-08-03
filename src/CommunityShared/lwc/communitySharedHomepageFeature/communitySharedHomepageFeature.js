5972a3656950e3bc8636583f5837ae37f062598f
Tim Lamond <tlamond@legendboats.com>
2021-08-03 12:16:22 -0400

    Reverting component to previous state to align with what is currently in staging/production; component in altered state was not being used, and therefore the changes can be skipped.

--- src/CommunityShared/lwc/communitySharedHomepageFeature/communitySharedHomepageFeature.js
+++ src/CommunityShared/lwc/communitySharedHomepageFeature/communitySharedHomepageFeature.js
@@ -2,38 +2,16 @@
  * Created by Tim on 2021-03-24.
  */
 
-import { LightningElement, api, wire } from 'lwc';
-import Id from '@salesforce/community/Id';
-import fetchCommunityDetails from '@salesforce/apex/CommSharedURL_Controller.fetchCommunityDetails';
-import { setWrapperClass } from 'c/communitySharedUtils';
+import { LightningElement, api, wire, track } from 'lwc';
 
 export default class CommunitySharedHomepageFeature extends LightningElement {
 	@api featureHeading;
 	@api featureBlurb;
 	@api featureThumb;
-	thumbnail;
 	@api buttonText;
 	@api buttonLink;
-	@api linkTarget;
-	@api sectionWidth;
 
-	wrapperClass = 'featureCard';
-
-	renderedCallback(){
-	  this.wrapperClass = setWrapperClass(this.sectionWidth, 'featureCard');
-
-		if(this.featureThumb){
-		  fetchCommunityDetails({communityId: Id})
-				.then( (result) => {
-					console.log('fetch community url result: ', result);
-					this.thumbnail = 'background-image: url("' + result.siteUrl + '/cms/delivery/media/' + this.featureThumb + '")';
-				 }).catch(e => {
-					 console.log('fetch community url error: ', e);
-				 });
-		}
-	}
-
-	get target(){
-		return (this.linkTarget == 'Default') ? '' : '_blank';
+	get thumbnail(){
+		return 'background-image: url(' + this.featureThumb + ')';
  	}
 }
