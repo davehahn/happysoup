9690fb7d0bd6070c96c223ee21a47b608141283c
Dave Hahn <dhahn@legendboats.com>
2022-04-05 15:15:22 -0400

    make a change to the auto PR request

--- build/circleci/create_pull_request.sh
+++ build/circleci/create_pull_request.sh
@@ -8,7 +8,7 @@ DEST_BRANCH=$1
 echo "Creating PR: $GIT_BRANCH -> $DEST_BRANCH"
 curl -X POST https://api.bitbucket.org/2.0/repositories/legend_devs/happysoup-sfdx/pullrequests \
   --fail --show-error --silent \
-  --user $BB_USER:$BB_PASSWORD \
+  -u $BB_USER:$BB_PASSWORD \
   -H 'content-type: application/json' \
   -d '{
     "title": "'$GIT_BRANCH' -> '$DEST_BRANCH'",
